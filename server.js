8883916379243bd0a19cb3558695546f6cb1eb21
nebras4u <nebras4u@gmail.com>
2025-06-13 14:14:20 +0330

    النسخة الأول المشروع

--- server.js
+++ server.js
@@ -34,27 +34,28 @@ socket.on('disconnect', () => {
         broadcastUsers();
     }
 });
-
+/*
     socket.on('join', (name) => {
         username = name;
         users.set(socket.id, name);
         socket.broadcast.emit('message', { user: '🟢 النظام', text: `${name} انضم إلى الدردشة.` });
         socket.emit('history', messages);
     });
-
+*/
     socket.on('chat message', (msg) => {
         const message = { user: username, text: msg };
         messages.push(message);
         if (messages.length > 20) messages.shift();
         io.emit('message', message);
     });
-
+/*
     socket.on('disconnect', () => {
         if (username) {
             users.delete(socket.id);
             socket.broadcast.emit('message', { user: '🔴 النظام', text: `${username} غادر الدردشة.` });
         }
     });
+*/
 });
 
 http.listen(PORT, () => {
